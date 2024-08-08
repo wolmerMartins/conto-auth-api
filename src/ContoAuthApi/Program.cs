950355e571a3b97f9c60cb09167c56d1f779634f
Wolmer Martins <wolmermartins@hotmail.com>
2024-08-08 17:37:59 -0300

    chore: configures cors for requests

--- src/ContoAuthApi/Program.cs
+++ src/ContoAuthApi/Program.cs
@@ -4,6 +4,18 @@ using ContoAuthApi.Accounts.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var MyCorsPolicy = "MyCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        name: MyCorsPolicy,
+        policy =>
+        {
+            policy.WithOrigins(["http://localhost:3000"]);
+        }
+    );
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -26,6 +38,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyCorsPolicy);
 
 app.MapControllers();
 
